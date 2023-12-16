7a9faa28e780cec416810f003a5d008a2041f9d4
qcmojimenez <124145250+qcmojimenez@users.noreply.github.com>
2023-12-16 19:35:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,48 @@
+# Names:
+#    Lomibao, Justin Joshua
+#    Genabe, Richmond John
+#    Jimenez, Maw
+#    Carl Voltair
+#    Lance Macamus
+# Course & Section: CPE019-CPE32S1
+# Instructor: Dr. Jonathan V. Taylar
+# Date Perform: December 10, 2023
+# Date Submitted: December 11, 2023
 import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
 
-def install_packages():
-    try:
-        import tensorflow
-    except ImportError:
-        st.warning("Installing TensorFlow...")
-        st.code("!pip install tensorflow", language="bash")
-        st.success("TensorFlow installed!")
+# Add the following lines to explicitly set _file_
+if _name_ == '_main_':
+    import _main_ as main
+    setattr(main, '_file_', 'app.py')  # Replace with your actual script name
 
-# Install required packages
-install_packages()
+# Define a dummy hash function for builtins.function
+def my_hash_func(func):
+    return hash(func.__code__)
 
-# Import the installed packages
-from tensorflow.keras.models import load_model
+@st.cache(allow_output_mutation=True, hash_funcs={type(lambda: None): my_hash_func})
+def load_model():
+    model = tf.keras.models.load_model('best_model.h5')
+    return model
 
-
-# Load the trained model
-model_path = '/best_model.h5'
-model = load_model(model_path)
-
-# Streamlit UI
 st.title("Emtech2 - Emotion Prediction App")
+
+model = load_model()
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
-    # Preprocess the image
     img = Image.open(uploaded_file)
     img = img.resize((64, 64))
-    img_array = image.img_to_array(img)
+    img_array = np.array(img)
+    img_array = img_array / 255.0
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0
 
-    # Make prediction
     prediction = model.predict(img_array)
-
-    # Display results
-    predicted_class = "Happy" if prediction[0] >= 0.5 else "Sad"
-    confidence = prediction[0] if predicted_class == "Happy" else 1 - prediction[0]
+    predicted_class = "Happy" if prediction[0, 0] >= 0.5 else "Sad"
+    confidence = prediction[0, 0] if predicted_class == "Happy" else 1 - prediction[0, 0]
     confidence_scalar = float(confidence)
 
     st.image(img, caption=f'Predicted Class: {predicted_class} (Confidence: {confidence_scalar:.2f})', use_column_width=True)
