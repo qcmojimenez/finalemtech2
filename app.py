7385913d9275b4ba52ca358b1bbaea739bbbe641
qcmojimenez <124145250+qcmojimenez@users.noreply.github.com>
2023-12-16 22:19:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@
 # Date Submitted: December 11, 2023
 import streamlit as st
 import numpy as np
-from keras.preprocessing import image
+from tensorflow.keras.preprocessing import image
 from keras.models import load_model
 from PIL import Image
 
