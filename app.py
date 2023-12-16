275ee7e650e654acdc13d3afd6451ece40993c51
qcmojimenez <124145250+qcmojimenez@users.noreply.github.com>
2023-12-16 22:23:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
-# Names:
-#    Lomibao, Justin Joshua
-#    Genabe, Richmond John
-#    Jimenez, Maw
-#    Carl Voltair
-#    Lance Macamus
-# Course & Section: CPE019-CPE32S1
-# Instructor: Dr. Jonathan V. Taylar
-# Date Perform: December 10, 2023
-# Date Submitted: December 11, 2023
 import streamlit as st
 import numpy as np
-from tensorflow.keras.preprocessing import image
-from keras.models import load_model
+from tensorflow.keras.preprocessing import image  # Explicit import for Keras image preprocessing
+from tensorflow.keras.models import load_model  # Explicit import for Keras model loading
+
+# PIL import for image manipulation
 from PIL import Image
 
-# Load the trained model
+# Load the trained model with updated path
 model_path = 'best_model.h5'  # Update with the correct path
 model = load_model(model_path)
 
@@ -26,9 +18,9 @@ st.title('Happy or Sad Detection')
 
 # Description
 st.markdown("""
-    The concept of the project is based on the midterm exam that identifies the weather.
-    We applied a CNN model to train the model for detecting if the face is happy or sad.
-    You can upload a photo we reserved from the Google Drive link that we also submitted.
+The concept of the project is based on the midterm exam that identifies the weather.
+We applied a CNN model to train the model for detecting if the face is happy or sad.
+You can upload a photo we reserved from the Google Drive link that we also submitted.
 """)
 
 # Upload image through Streamlit
@@ -40,7 +32,7 @@ if uploaded_file is not None:
     st.image(image_display, caption='Uploaded Image.', use_column_width=True)
 
     # Preprocess the image for the model
-    img = image.load_img(uploaded_file, target_size=(64, 64))
+    img = image.load_img(uploaded_file, target_size=(64, 64))  # Use Keras image preprocessing
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0
