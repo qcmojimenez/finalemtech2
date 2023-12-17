23dcadf1d973bf53d9c05d0f3640b2132605fea3
qcmojimenez <124145250+qcmojimenez@users.noreply.github.com>
2023-12-17 09:51:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+!pip install --upgrade pip
 import os
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, load_model
@@ -8,6 +9,7 @@ from keras.preprocessing import image
 import numpy as np
 import streamlit as st
 
+model.save(os.path.join(save_dir, 'best_model.h5'))
 loaded_model = load_model(os.path.join(save_dir, 'best_model.h5'))
 st.title("Emotion Detection Streamlit App")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
@@ -22,5 +24,3 @@ if uploaded_file is not None:
     prediction = loaded_model.predict(img_array)
     st.write(f"Predicted Probability: {prediction[0]}")
     st.write(f"Predicted Class: {round(prediction[0][0])}")
-
-
